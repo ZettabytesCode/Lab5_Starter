91dcdcfecda1e0527fce5b2b9acc79bdb26da5ae
Combobyte <varun.sreedhar1@gmail.com>
2023-05-03 15:31:29 -0700

    Added image change

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,14 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+const hornSelector = document.getElementById("horn-select");
+hornSelector.addEventListener('change', updateHornImage);
+
 function init() {
   // TODO
+}
+
+function updateHornImage(horn){
+  var image = document.querySelector("img");
+  image.src = horn + ".svg";
 }
