3594e1e7d088014097e31a6a0a542963634aa1a7
Combobyte <varun.sreedhar1@gmail.com>
2023-05-03 16:47:45 -0700

    finished explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,12 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+const synth = window.speechSynthesis;
 
+const speakButton = document.querySelector("button");
+speakButton.addEventListener('click', speakText);
+
+setInterval(faceSwap, 500);
 
 function init() {
   const voices = speechSynthesis.getVoices();
@@ -13,5 +18,20 @@ function init() {
     voice_option.setAttribute("name", voices[i].name);
 
     document.getElementById("voice-select").appendChild(voice_option);
+  }
+}
+
+function speakText(event){
+  const text = document.querySelector("textarea");
+  const utterance = new SpeechSynthesisUtterance(text.value);
+  synth.speak(utterance);
+}
+
+function faceSwap(){
+  const image = document.querySelector("img");
+  if(synth.speaking){
+    image.setAttribute("src", "/assets/images/smiling-open.png");
+  }else{
+    image.setAttribute("src", "/assets/images/smiling.png");
   }
 }
