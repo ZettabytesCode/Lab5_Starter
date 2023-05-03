7904af9417751fae8d82f1c8b1ce89dd8145100e
Combobyte <varun.sreedhar1@gmail.com>
2023-05-03 15:56:21 -0700

    finished image

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -5,11 +5,16 @@ window.addEventListener('DOMContentLoaded', init);
 const hornSelector = document.getElementById("horn-select");
 hornSelector.addEventListener('change', updateHornImage);
 
+const volumeController = document.getElementById("volume");
+
+
 function init() {
   // TODO
 }
 
 function updateHornImage(event){
   var image = document.querySelector("img");
+  var audio = document.querySelector("audio");
   image.setAttribute("src", `/assets/images/${event.target.value}.svg`);
+  audio.setAttribute("src", `/assets/audio/${event.target.value}.mp3`);
 }
