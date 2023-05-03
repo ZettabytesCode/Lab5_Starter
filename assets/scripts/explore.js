c12e63adfc074e309453e54e4f7044cb9d66285c
Combobyte <varun.sreedhar1@gmail.com>
2023-05-03 16:35:49 -0700

    added voice list

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,16 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
-  // TODO
+  const voices = speechSynthesis.getVoices();
+  for(let i = 0; i < voices.length; i++){
+    const voice_option = document.createElement("voice_option");
+    voice_option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    voice_option.setAttribute("lang", voices[i].lang);
+    voice_option.setAttribute("name", voices[i].name);
+
+    document.getElementById("voice-select").appendChild(voice_option);
+  }
 }
